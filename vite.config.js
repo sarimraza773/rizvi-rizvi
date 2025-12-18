a6cd38b4aa1e906e355146d5ece51f9295f3b041
Syed Sarim Raza <sarimraza773@gmail.com>
2025-12-18 18:44:26 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // Set the base path to '/' by default.  If you deploy to GitHub Pages under
   base: '/',
 });
